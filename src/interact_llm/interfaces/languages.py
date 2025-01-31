a08412eec9076c9f2fd047ef14d8f56c0988b7a8
MinaAlmasi <mina.almasi00@gmail.com>
2025-01-31 14:09:57 +0100

    feat: add supported languages

---
+++ src/interact_llm/interfaces/languages.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import Dict
+
+@dataclass
+class Language:
+    code: str
+    name: Dict[str, str]
+
+supported_languages = [
+    Language(
+        code="es",
+        name={
+            "english": "Spanish",
+            "danish": "Spansk",
+            "native": "Español"
+        }
+    ),
+    Language(
+        code="de",
+        name={
+            "english": "German",
+            "danish": "Tysk",
+            "native": "Deutsch"
+        }
+    )
+]
+
+if __name__ == "__main__":
+    print("[INFO:] Supporting:")
+    for i, lang in enumerate(supported_languages):
+        print("{0}. {1} ({2})".format(i+1, lang.name["english"], lang.code))
